09c5519250c4f4ad5e66796dc00e4524cde7cbe9
Cycl0matic <muhammadalaidy@protonmail.com>
2020-10-29 01:44:26 +0200

    add sell/buy from coinbase methods to transaction model

--- src/transaction/model.js
+++ src/transaction/model.js
@@ -55,7 +55,7 @@ TransactionSchema.statics.sellQuote = function ({ total }) {
       account.sell({ total, cyrrency: "BTC", quote: true }, (err, tx) => {
         if (err) {
           rej(err);
-        }   
+        }
         res({
           amount: tx.amount,
           cost: tx.total,
@@ -63,4 +63,52 @@ TransactionSchema.statics.sellQuote = function ({ total }) {
       });
     });
   });
+};
+
+// buy from coinbase or an exchange
+TransactionSchema.statics.buy = async function ({ price, amount }) {
+  const Wallet = mongoose.model("Wallet");
+  const wallet = await Wallet.findOne({ "balance.currency": "GBP" });
+  if (wallet.balance.amount < price) {
+    throw new Error("Can;'t afford that price");
+  }
+
+  const trans = await Transaction.create({
+    currency: "BTC",
+    amount,
+    type: "buy",
+    price,
+    created: new Date(),
+  });
+
+  await Promise.all([
+    Wallet.addToCurrency({ type: "BTC", amount: amount }),
+    Wallet.addToCurrency({ type: "GBP", amount: -price }),
+  ]);
+
+  return trans;
+};
+
+// sell via coinbase
+TransactionSchema.statics.sell = async function ({ price, amount }) {
+  const Wallet = mongoose.model("Wallet");
+  const wallet = await Wallet.findOne({ "balance.type": "BTC" });
+  if (wallet.balance.amount < amount) {
+    throw new Error("you don't have enough to sell");
+  }
+
+  const trans = await Transaction.create({
+    currency: "BTC",
+    amount,
+    type: "sell",
+    price,
+    created: new Date(),
+  });
+
+  await Promise.all([
+    Wallet.addToCurrency({ type: "BTC", amount: -amount }),
+    Wallet.addToCurrency({ type: "GBP", amount: price }),
+  ]);
+
+  return trans;
 };
